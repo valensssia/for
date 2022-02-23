91c268ab2245a62da5a890497e5775413649ba86
valensssia <99199656+valensssia@users.noreply.github.com>
2022-02-23 21:35:32 +0200

    Add files via upload

---
+++ zalyshok.java
@@ -0,0 +1,20 @@
+package forr;
+
+import java.util.Scanner;
+
+public class zalyshok {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		for (int i = a; i <= b; i++) {
+			if (i % d == c) {
+				System.out.println(i);
+			}
+		}
+			}
+		
+
+}
