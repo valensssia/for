6f8481b79f561aa45ed592d24a671e2fc5c06f76
valensssia <99199656+valensssia@users.noreply.github.com>
2022-02-23 21:41:30 +0200

    Add files via upload

---
+++ pereverny_chyslo.java
@@ -0,0 +1,18 @@
+package forr;
+
+import java.util.Scanner;
+
+public class pereverny_chyslo {
+	public static void main(String[] args){
+		Scanner sc = new Scanner(System.in);
+		int x = sc.nextInt();
+		int s = 0;
+		for (int i = 1; x != 0; i++) {
+			s = x % 10;
+			x /= 10;
+			System.out.print(s);
+		}
+	}
+
+
+}
